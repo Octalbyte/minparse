8590c824eea8ec1c02a87d7398df19f363d17c2c
JPSO <jpso.guerreiro@gmail.com>
2022-01-21 14:52:49 +0000

    add_fn_subcommands

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -7,6 +9,30 @@ mod tests {
     }
 }
 
-pub fn parse(){
+pub mod parse {
 
+    use std::env;
+
+    pub fn process_name() -> String {
+        return env::args().collect();
+    }
+
+    pub fn subcommands() -> Vec<String>{
+        let mut subcommands: Vec<String> = vec![];
+        for i in env::args() {
+            if i.starts_with("--") {
+                break;
+            }
+            subcommands.push(i);
+        }
+        return subcommands;
+    }
+
+    pub fn switches() -> Vec<String> {
+        let mut switches: Vec<String> = vec![];
+        let args: Vec<String> = env::args().collect();
+        for i in 0..args.len(){
+            
+        }
+    }
 }
